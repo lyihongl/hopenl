94ebc5b691902d11ff78506a0649670b259cf234
Yihong Liu <yihongliu00@gmail.com>
2021-02-03 22:40:36 -0700

    hello world

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for hopengl2
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ src/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import Graphics.UI.GLUT
+
+myPoints :: [(GLfloat, GLfloat, GLfloat)]
+myPoints = [(sin (2 * pi * k / 12), cos (2 * pi * k / 12), 0) | k <- [1 .. 12]]
+
+main :: IO ()
+main = do
+  (_progName, _args) <- getArgsAndInitialize
+  _window <- createWindow "Hello World"
+  displayCallback $= display
+  mainLoop
+
+display :: DisplayCallback
+display = do
+  clear [ColorBuffer]
+  renderPrimitive TriangleStrip $
+    mapM_ (\(x, y, z) -> vertex (Vertex3 x y z)) myPoints
+  flush
